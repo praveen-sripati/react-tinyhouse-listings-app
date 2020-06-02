9cfd061a7927988cea13836a95037d8c459b0368
Praveen <pvnsripati@gmail.com>
2020-06-03 01:50:37 +0530

    initial commit

---
+++ client/src/sections/Listings/Listings.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface Props {
+  title: string;
+}
+
+export const Listings = ({ title }: Props) => {
+  return <h1>{title}</h1>
+}
---
+++ server/src/index.ts
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+import express, { Application } from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { connectDatabase } from './database';
+import { typeDefs, resolvers } from './graphql';
+
+// const port = 9000;
+
+const mount = async (app: Application) => {
+  const db = await connectDatabase();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: { db }
+  });
+  server.applyMiddleware({app, path: '/api'});
+
+  app.listen(process.env.PORT, () => console.log(`Port is ${process.env.PORT} running!`));
+
+  const listings = await db.listings.find({}).toArray();
+  console.log(listings);
+}
+
+mount(express());
