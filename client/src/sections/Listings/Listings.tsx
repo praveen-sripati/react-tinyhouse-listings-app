cdec050e16a20f74e98addb122fee562da735818
Praveen <pvnsripati@gmail.com>
2020-06-03 02:40:29 +0530

    Querying Listings

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { server } from '../../lib/api';
+import { ListingData } from './types'
+
+const LISTINGS = `
+  query Listings {
+    listings {
+      id
+      title
+      image
+      address
+      price
+      numOfGuests
+      numOfBeds
+      numOfBaths
+      rating
+    }
+  }
+`;
 
 interface Props {
   title: string;
 }
 
 export const Listings = ({ title }: Props) => {
-  return <h1>{title}</h1>
+
+  const fetchListings = async () => {
+    const { data } =
+     await server.fetch<ListingData>( {query:LISTINGS} );
+    console.log(data);
+  }
+  return (
+    <div>
+      <h1>{title}</h1>
+      <button onClick={fetchListings}>Query Listings!</button>
+    </div>
+  )
 }
